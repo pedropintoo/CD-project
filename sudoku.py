0c8b9309a2af0a19406730126407880639e187d4
Diogo Gomes <dgomes@ua.pt>
2024-04-18 22:24:22 +0100

    Ready for students

--- sudoku.py
+++ sudoku.py
@@ -1,6 +1,11 @@
+import time
+from collections import deque
+
 class Sudoku:
     def __init__(self, sudoku):
         self.grid = sudoku
+        self.recent_requests = deque()
+
 
     def __str__(self):
         string_representation = "| - - - - - - - - - - - |\n"
@@ -17,7 +22,20 @@ class Sudoku:
 
         return string_representation
 
-    def check(self):
+    def check(self, base_delay=0.01, interval=10, threshold=5):
+        """Check if the given Sudoku solution is correct.
+        
+        You MUST incorporate this method without modifications into your final solution.
+        """
+
+        current_time = time.time()
+        self.recent_requests.append(current_time)
+        num_requests = len([t for t in self.recent_requests if current_time - t < interval])
+
+        if num_requests > threshold:
+            delay = base_delay * (num_requests - threshold + 1)  # Increase delay based on excess requests
+            time.sleep(delay)
+
         # Check rows
         for row in range(9):
             if sum(self.grid[row]) != 45:
@@ -36,58 +54,8 @@ class Sudoku:
 
         return True
 
-    def solve(self):
-        empty_cell = self.__find_empty_cell()
-        if not empty_cell:
-            return True  # Puzzle solved
 
-        row, col = empty_cell
-        for num in range(1, 10):
-            if self.__is_valid_move(row, col, num):
-                self.grid[row][col] = num
-                if self.solve():
-                    return True
-                self.grid[row][col] = 0  # Backtrack
-        return False
-
-    def __find_empty_cell(self):
-        for i in range(9):
-            for j in range(9):
-                if self.grid[i][j] == 0:
-                    return (i, j)
-        return None
-
-    def __is_valid_move(self, row, col, num):
-        # Check row
-        if num in self.grid[row]:
-            return False
-
-        # Check column
-        if num in [self.grid[i][col] for i in range(9)]:
-            return False
-
-        # Check 3x3 square
-        start_row, start_col = 3 * (row // 3), 3 * (col // 3)
-        for i in range(start_row, start_row + 3):
-            for j in range(start_col, start_col + 3):
-                if self.grid[i][j] == num:
-                    return False
-
-        return True
-
-
-def main():
-    solved_sudoku = [
-        [8,2,7,1,5,4,3,9,6],
-        [9,6,5,3,2,7,1,4,8],
-        [3,4,1,6,8,9,7,5,2],
-        [5,9,3,4,6,8,2,7,1],
-        [4,7,2,5,1,3,6,8,9],
-        [6,1,8,9,7,2,4,3,5],
-        [7,8,6,2,3,5,9,1,4],
-        [1,5,4,7,9,6,8,2,3],
-        [2,3,9,8,4,1,5,6,7]
-    ]
+if __name__ == "__main__":
 
     sudoku = Sudoku([
         [0,0,0,1,0,0,0,0,0],
@@ -103,11 +71,7 @@ def main():
 
     print(sudoku)
 
-    sudoku.solve()
-
     if sudoku.check():
         print("Sudoku is correct!")
-        print(sudoku)
-
-if __name__ == "__main__":
-    main()
+    else:
+        print("Sudoku is incorrect! Please check your solution.")
