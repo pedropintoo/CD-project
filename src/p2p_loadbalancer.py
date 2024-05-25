7f73480a2dd00716512e69540b142178d7b9a454
Pedro Pinto <pmap@ua.pt>
2024-05-25 20:35:51 +0100

    improve loadbalancer logic

--- src/p2p_loadbalancer.py
+++ src/p2p_loadbalancer.py
@@ -8,80 +8,102 @@ class Worker:
         self.socket = socket
 
         # availability
-        self.last_signal = time.time()
-        self.Alive = True
-        self.isAvailable = True
-        self.isTheFirstResponse = True
+        self.Alive = True       # false when worker is dead ( it means that the worker is not responding or socket was closed )
+        self.isAvailable = True # false when worker is working
 
-        # response time
-        self.ema_response_time = 10.0  # Exponential Moving Average response time TODO: this initial value is arbitrary!
+        # hello response time
+        self.isFirstHello = True
+        self.last_hello_received = time.time()
+        self.hello_response_time = 10.0  # Exponential Moving Average response time 
+
+        # task response time
+        self.isFirstTask = True
+        self.last_task_sended = time.time()
+        self.task_response_time = 10.0   # Exponential Moving Average response time
+
+        # response time factor
         self.smoothing_factor = smoothing_factor # TODO: increase when worker gets older (more stable)
 
+    def start_task(self):
+        """Worker start a task."""
+        self.isAvailable = False
+        self.last_task_sended = time.time()
 
-    def update_ema_response_time(self, response_time: float):
-        """Update the EMA response time given a new response time."""
-        if self.isTheFirstResponse:
-            self.ema_response_time = response_time
-            self.isTheFirstResponse = False
-        else:
-            self.ema_response_time = (self.smoothing_factor * response_time +
-                                      (1 - self.smoothing_factor) * self.ema_response_time)
-
-    def alive_signal(self):  
+    def hello_received(self):  
         """Worker give signs of aliveness.""" 
         self.Alive = True
-        self.last_signal = time.time()
+        self.update_hello_response_time()
 
-    def checkAlive(self):
+    def task_done(self):  
+        """Worker give signs of aliveness.""" 
+        self.isAvailable = True
+        self.update_task_response_time()
+
+    def update_hello_response_time(self):
+        """Update the hello response time given a new response."""
+        elapsed_time = time.time() - self.last_hello_received
+        if self.isFirstHello:
+            self.hello_response_time = elapsed_time
+            self.isFirstHello = False
+        else:
+            self.hello_response_time = (self.smoothing_factor * elapsed_time +
+                                      (1 - self.smoothing_factor) * self.hello_response_time)
+    
+    def update_task_response_time(self):
+        """Update the task response time given a new response."""
+        elapsed_time = time.time() - self.last_hello_received
+        if self.isFirstTask:
+            self.task_response_time = elapsed_time
+            self.isFirstTask = False
+        else:
+            self.task_response_time = (self.smoothing_factor * elapsed_time +
+                                      (1 - self.smoothing_factor) * self.task_response_time)
+
+    def checkHelloTimeout(self):
         if self.Alive == False:
             return False
 
         # Recalculate
-        now = time.time()
-        elapsed_time = (now - self.last_signal)
-        if elapsed_time > 10: # TODO: thing about this limit value
+        elapsed_time = time.time() - self.last_signal
+        if elapsed_time > 5 * self.hello_response_time: # TODO: thing about this limit value
             self.Alive = False
         
         return self.Alive
     
+    def checkTaskTimeout(self):
+        elapsed_time = time.time() - self.last_task_sended
+        return elapsed_time > 2 * self.task_response_time
+
     def crash(self):
-        """Crash the worker -> worker is not alive."""
+        """Crash the worker"""
         self.Alive = False
         self.isAvailable = True # for future reconnection
-
+        self.last_hello_received = time.time()
+        self.last_task_sended = time.time()
 
 
 class Task:
     def __init__(self, task_id: int, worker: Worker, tries_limit: int = 1):
         self.task_id = task_id
-        self.worker = worker
-        worker.isAvailable = False
-        self.start_time = time.time()
-        self.tries = 0
         
-        # Limit parameters
+        self.worker = worker
+        worker.start_task()
+        
+        # Limit retries of the task
+        self.tries = 0
         self.tries_limit = tries_limit
 
     def has_timed_out(self) -> bool:
         """Check if the task has exceeded its time limit."""
-        return time.time() - self.start_time > (self.worker.ema_response_time*2)
+        return not self.worker.Alive or self.worker.checkTaskTimeout()
 
     def has_exceeded_tries(self) -> bool:
         """Check if the task has exceeded its retry limit."""
         return self.tries >= self.tries_limit
 
     def retry(self):
-        """Increment the number of tries and reset the start time."""
+        """Increment the number of tries."""
         self.tries += 1
-        now = time.time()
-        #self.worker.update_ema_response_time( now - self.start_time )
-        self.start_time = now   
-        
-    def end(self):
-        """End the task."""
-        self.worker.isAvailable = True
-        now = time.time()
-        self.worker.update_ema_response_time( now - self.start_time )
 
 
 # Workers & Tasks Manager (load balancer)
@@ -103,11 +125,12 @@ class WTManager:
         task = self.working_tasks.get(int(task_id)) 
         
         if task is not None:
-            task.end() # update worker
+            task.worker.task_done()
             del self.working_tasks[task_id]
         else:
             try:
-                self.pending_tasks_queue.remove(task_id) # the responser is a dead worker TODO: actualize the worker!!!
+                if task_id in self.pending_tasks_queue:
+                    self.pending_tasks_queue.remove(task_id) # the responser is a dead worker
             except ValueError:
                 pass 
 
@@ -128,14 +151,14 @@ class WTManager:
         worker = self.workersDict.get(host_port)
         worker.crash()
 
-        # close the socket if needed
+        # close the socket if connection to worker fail!
         if close_socket:
             worker.socket.close()
             worker.socket = None
         
         # unsign every task of the dead worker
-        tasks = list(self.working_tasks.values()).copy()
-        for task in tasks:
+        tasks_copy = list(self.working_tasks.values()).copy() # must be a copy because we are deleting elements!
+        for task in tasks_copy:
             if task.worker == worker:
                 self.unassign_task(task) 
 
@@ -156,26 +179,27 @@ class WTManager:
         """Get the list of alive workers addresses."""
         return [worker.worker_address for worker in self.get_alive_workers()]
 
-    def checkWorkersTimeouts(self):
-        """Check for workers that not make signal of aliveness and handle retries."""
+    def checkWorkersHelloTimeouts(self):
+        """Check for workers that have timed out."""
         for worker in self.get_alive_workers():
             if not worker.checkAlive():
-                # we do not kill the socket
+                # we do not kill the socket! we just mark the worker as dead
                 self.kill_worker(worker.worker_address, close_socket=False)
 
     def checkTasksTimeouts(self) -> list[Task]:
         """Check for tasks that have timed out and handle retries."""        
-        # tasks
-        timeout_tasks = []
-        for task in list(self.working_tasks.values()):
-            if task.has_timed_out(): # TODO: worker response timeout!
+        # tasks to retry
+        retry_tasks = []
+        tasks_copy = list(self.working_tasks.values()).copy() # must be a copy because we are deleting elements!
+        for task in tasks_copy:
+            if task.has_timed_out():
                 if task.has_exceeded_tries():
                     # task expired
                     self.unassign_task(task) # add to pending queue
                 else:
-                    timeout_tasks.append(task) # client must retry !!
+                    retry_tasks.append(task) # client must retry !!
 
-        return timeout_tasks   
+        return retry_tasks   
 
     def get_best_worker(self) -> Worker:
         """Get the worker with the lowest EMA response time."""
@@ -187,19 +211,19 @@ class WTManager:
 
     def get_tasks_to_work(self) -> List[Task]:
         """Get new tasks with associated workers."""
-        new_tasks = []
+        new_work_tasks = []
         pending = self.pending_tasks_queue.copy()
         for task_id in pending:
             worker = self.get_best_worker()
             if worker is not None:
                 task = Task(task_id, worker)
-                new_tasks.append(task)
+                new_work_tasks.append(task)
                 self.working_tasks[task_id] = task
                 self.pending_tasks_queue.remove(task_id)
             else:
                 break # no more workers available
 
-        return new_tasks
+        return new_work_tasks
 
 
 
